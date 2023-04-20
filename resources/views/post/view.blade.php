6cc4d412381a3ab6153ec588b56b563f508b21a6
Sergey <41381316+Sergeyopt@users.noreply.github.com>
2023-04-20 09:27:29 +0300

    Paginate post
    Реализация пагинации во время просмотра постов.

--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -14,7 +14,26 @@
                     <p class="post-description">{{ strip_tags(str_replace('&nbsp;', '', $post->description)) }}</p>
                 </div>
             </div>
+            <div class="navigation-post">
+                @if($prev)
+                    <div class="navigation-prev">
+                        <a class="nav-link--prev" href="{{ route('view', $prev) }}">
+                            <h3>&#10229; Previous</h3>
+                            <p>{{ Str::words($prev->title, 25) }}</p>
+                        </a>
+                    </div>
+                @endif
+                @if($next)    
+                    <div class="navigation-next">
+                        <a class="nav-link--next" href="{{ route('view', $next) }}">
+                            <h3 class="nav-next-title">Next &#10230;</h3>
+                            <p>{{ Str::words($next->title, 10, '...') }}</p>
+                        </a>
+                    </div>
+                @endif
+            </div>
         </section>
+        
     </div>
     <div class="col-md-3">
         <x-sidebar />
